d5a77a0bd3e14046f96751a38a8c3f09cc9bd7ac
096benjaminbenoit <096benjaminbenoit@gmail.com>
2023-12-28 12:17:16 +0100

    reset mes bétises

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -23,7 +23,7 @@ export default function Contact() {
                   Nous envoyer un message :
                 </h2>
 
-                <form action="/contact.php" method="POST">
+                <form action="" method="POST">
                   <div className="grid gap-4">
                     <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
                       <div>
